53419ca13893e5a8ab9b53e119c8bfc07edae23a
siddharth2440 <harshitsingh8427.live@gmail.com>
2026-08-31 23:55:46 +0530

    matrix op. done

--- src/main.zig
+++ src/main.zig
@@ -10,11 +10,11 @@ pub fn main(init: std.process.Init) !void {
     var arena = init.arena;
     const allocator = arena.allocator();
 
-    var i_mat = try Matrix.identity_matrix(4, allocator);
-    std.debug.print("{any}\n", .{i_mat.data});
+    // var i_mat = try Matrix.identity_matrix(4, allocator);
+    // std.debug.print("{any}\n", .{i_mat.data});
 
-    i_mat.multiply_by(2);
-    std.debug.print("{any}\n", .{i_mat.data});
+    // i_mat.multiply_by(2);
+    // std.debug.print("{any}\n", .{i_mat.data});
 
     var mat = try Matrix.init(2, 3, allocator);
 
@@ -25,8 +25,56 @@ pub fn main(init: std.process.Init) !void {
     mat.set_value(2, 0, 15);
     mat.set_value(2, 1, 16);
 
-    mat.print_matrix();
+    // std.debug.print("Mat1: \n", .{});
+    // mat.print_matrix();
+    // std.debug.print("\n\n", .{});
 
-    var transpose = try mat.matrix_transpose();
-    transpose.print_matrix();
+    // var transpose = try mat.matrix_transpose();
+    // transpose.print_matrix();
+
+    var mat2 = try Matrix.init(2, 3, allocator);
+
+    mat2.set_value(0, 0, 11);
+    mat2.set_value(0, 1, 12);
+    mat2.set_value(1, 0, 13);
+    mat2.set_value(1, 1, 14);
+    mat2.set_value(2, 0, 15);
+    mat2.set_value(2, 1, 16);
+
+    // std.debug.print("Mat2: \n", .{});
+    // mat2.print_matrix();
+    // std.debug.print("\n\n", .{});
+
+    // var mat_addition_result = try mat.matrix_addition(&mat2) orelse return error.InvalidMatrixDimension;
+    // std.debug.print("Matrix Addition: \n", .{});
+    // mat_addition_result.print_matrix();
+
+    var mmat1 = try Matrix.init(3, 2, allocator);
+    mmat1.set_value(0, 0, 11);
+    mmat1.set_value(0, 1, 12);
+    mmat1.set_value(0, 2, 13);
+    mmat1.set_value(1, 0, 14);
+    mmat1.set_value(1, 1, 15);
+    mmat1.set_value(1, 2, 16);
+
+    std.debug.print("MMat1 dim: ({d} x {d})\n", .{ mmat1.rows, mmat1.cols });
+    mmat1.print_matrix();
+    std.debug.print("\n\n", .{});
+
+    var mmat2 = try Matrix.init(2, 3, allocator);
+
+    mmat2.set_value(0, 0, 21);
+    mmat2.set_value(0, 1, 22);
+    mmat2.set_value(1, 0, 23);
+    mmat2.set_value(1, 1, 24);
+    mmat2.set_value(2, 0, 25);
+    mmat2.set_value(2, 1, 26);
+
+    std.debug.print("MMat2 dim: ({d} x {d})\n", .{ mmat2.rows, mmat2.cols });
+    mmat2.print_matrix();
+    std.debug.print("\n\n", .{});
+
+    var mat_multiplication_result = try mmat1.matrix_multiplication(&mmat2) orelse return error.InvalidMatrixDimension;
+    std.debug.print("Matrix Multiplication: \n", .{});
+    mat_multiplication_result.print_matrix();
 }
--- src/root.zig
+++ src/root.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const Io = std.Io;
 
 pub const Matrix = struct {
-    rows: u8,
-    cols: u8,
-    data: []u8,
+    rows: u32,
+    cols: u32,
+    data: []u32,
     allocator: std.mem.Allocator,
 
-    pub fn init(cols: u8, rows: u8, allocator: std.mem.Allocator) !Matrix {
-        const data = try allocator.alloc(u8, rows * cols);
+    pub fn init(cols: u32, rows: u32, allocator: std.mem.Allocator) !Matrix {
+        const data = try allocator.alloc(u32, rows * cols);
 
         return .{
             .cols = cols,
@@ -18,26 +18,26 @@ pub const Matrix = struct {
         };
     }
 
-    pub inline fn get_value(self: *Matrix, row: u32, col: u32) u8 {
+    pub inline fn get_value(self: *Matrix, row: u32, col: u32) u32 {
         return self.data[(self.cols * row) + col];
     }
 
-    pub inline fn set_value(self: *Matrix, row: u32, col: u32, value: u8) void {
+    pub inline fn set_value(self: *Matrix, row: u32, col: u32, value: u32) void {
         self.data[(self.cols * row) + col] = value;
     }
 
-    pub fn multiply_by(self: *Matrix, num: u8) void {
+    pub fn multiply_by(self: *Matrix, num: u32) void {
         for (self.data) |*value| {
             value.* *= num;
         }
     }
 
-    pub fn zero_matrix(rows: u8, cols: u8, allocator: std.mem.Allocator) !void {
+    pub fn zero_matrix(rows: u32, cols: u32, allocator: std.mem.Allocator) !void {
         const z_mat = try Matrix.init(cols, rows, allocator);
         @memset(z_mat.data, 0);
     }
 
-    pub fn identity_matrix(size: u8, allocator: std.mem.Allocator) !Matrix {
+    pub fn identity_matrix(size: u32, allocator: std.mem.Allocator) !Matrix {
         var i_mat = try Matrix.init(size, size, allocator);
         @memset(i_mat.data, 0);
 
@@ -64,12 +64,60 @@ pub const Matrix = struct {
         return transpose_matrix;
     }
 
+    pub fn matrix_addition(self: *Matrix, with: *Matrix) !?Matrix {
+        if (self.rows == with.rows and self.cols == with.cols) {
+            var result_matrix = try Matrix.init(self.cols, self.rows, self.allocator);
+
+            var row: u32 = 0;
+            while (row < self.rows) : (row += 1) {
+                var col: u32 = 0;
+                while (col < self.cols) : (col += 1) {
+                    const addtn_result = self.get_value(row, col) + with.get_value(row, col);
+
+                    result_matrix.set_value(row, col, addtn_result);
+                }
+            }
+
+            return result_matrix;
+        } else {
+            std.debug.print("Error: Dimensions should be identical. \n", .{});
+            return null;
+        }
+    }
+
+    pub fn matrix_multiplication(self: *Matrix, with: *Matrix) !?Matrix {
+        if (self.cols == with.rows) {
+            var resultant_matrix = try Matrix.init(with.cols, self.rows, self.allocator);
+            @memset(resultant_matrix.data, 0);
+
+            var row: u32 = 0;
+            while (row < self.rows) : (row += 1) {
+                var col: u32 = 0;
+                while (col < with.cols) : (col += 1) {
+                    var size: u32 = 0;
+                    while (size < self.cols) : (size += 1) {
+                        const cur_val = self.get_value(row, size);
+                        const other_matrix_val = with.get_value(size, col);
+                        const product = cur_val * other_matrix_val;
+                        resultant_matrix.set_value(row, col, resultant_matrix.get_value(row, col) + product);
+                    }
+                }
+            }
+
+            return resultant_matrix;
+        } else {
+            std.debug.print("Error: Matrix Dimension error", .{});
+            return null;
+        }
+    }
+
     pub fn print_matrix(self: *Matrix) void {
         var row: u32 = 0;
         while (row < self.rows) : (row += 1) {
             var col: u32 = 0;
             while (col < self.cols) : (col += 1) {
-                self.get_value(row, col);
+                const val = self.get_value(row, col);
+                std.debug.print("{d} \t", .{val});
             }
 
             std.debug.print("\n", .{});
